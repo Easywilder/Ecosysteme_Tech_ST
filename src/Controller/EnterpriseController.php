90033d53a105796c958da9dd49ec678798489a94
Laure Bluzet <laure.bluzet@gmail.com>
2020-01-27 12:57:29 +0100

    création du CRUD pour 5 entitées

---
+++ src/Controller/EnterpriseController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Enterprise;
+use App\Form\EnterpriseType;
+use App\Repository\EnterpriseRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/enterprise")
+ */
+class EnterpriseController extends AbstractController
+{
+    /**
+     * @Route("/", name="enterprise_index", methods={"GET"})
+     * @param EnterpriseRepository $enterpriseRepository
+     * @return Response
+     */
+    public function index(EnterpriseRepository $enterpriseRepository): Response
+    {
+        return $this->render('enterprise/index.html.twig', [
+            'enterprises' => $enterpriseRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="enterprise_new", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $enterprise = new Enterprise();
+        $form = $this->createForm(EnterpriseType::class, $enterprise);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($enterprise);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('enterprise_index');
+        }
+
+        return $this->render('enterprise/new.html.twig', [
+            'enterprise' => $enterprise,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="enterprise_show", methods={"GET"})
+     * @param Enterprise $enterprise
+     * @return Response
+     */
+    public function show(Enterprise $enterprise): Response
+    {
+        return $this->render('enterprise/show.html.twig', [
+            'enterprise' => $enterprise,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="enterprise_edit", methods={"GET","POST"})
+     * @param Request $request
+     * @param Enterprise $enterprise
+     * @return Response
+     */
+    public function edit(Request $request, Enterprise $enterprise): Response
+    {
+        $form = $this->createForm(EnterpriseType::class, $enterprise);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('enterprise_index');
+        }
+
+        return $this->render('enterprise/edit.html.twig', [
+            'enterprise' => $enterprise,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="enterprise_delete", methods={"DELETE"})
+     * @param Request $request
+     * @param Enterprise $enterprise
+     * @return Response
+     */
+    public function delete(Request $request, Enterprise $enterprise): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$enterprise->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($enterprise);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('enterprise_index');
+    }
+}
---
+++ src/Form/EnterpriseType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Enterprise;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EnterpriseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('adress')
+            ->add('city')
+            ->add('workforce')
+            ->add('activity')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Enterprise::class,
+        ]);
+    }
+}
